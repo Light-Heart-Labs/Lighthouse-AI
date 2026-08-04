Osmantic/ODS

refactor(dashboard-api): consolidate duplicated .env single-key readers
config._read_env_from_file, gpu._read_env_var_from_file and
performance_oracle.read_env_file_value were three behaviorally identical helpers
that scanned <install_dir>/.env line by line for one KEY=value entry. Extract
the logic into a new stdlib-only leaf module env_file.read_env_file_value and
have all three delegate to it.

Behavior preserved: same first-'='-split parse, same surrounding-quote strip,
same empty-on-missing fallback. The two callers that previously relied on the
platform-default text encoding now read the file as UTF-8 explicitly, matching
performance_oracle's prior behavior and the Linux container's default locale.

env_file stays dependency-free so the low-level config module imports it with no
risk of an import cycle. Adds tests/test_env_file.py (8 cases).

ods/extensions/services/dashboard-api/gpu.py
@@ -9,6 +9,7 @@ import subprocess
 from pathlib import Path
 from typing import Optional
 
+from env_file import read_env_file_value
 from models import GPUInfo, IndividualGPU
 
 logger = logging.getLogger(__name__)
@@ -422,14 +423,7 @@ def decode_gpu_assignment() -> Optional[dict]:
 def _read_env_var_from_file(key: str) -> str:
     """Read a single variable directly from the .env file (split on first '=' only)."""
     install_dir = os.environ.get("ODS_INSTALL_DIR", os.path.expanduser("~/ods"))
-    env_path = Path(install_dir) / ".env"
-    try:
-        for line in env_path.read_text().splitlines():
-            if line.startswith(f"{key}="):
-                return line[len(key) + 1:].strip().strip("\"'")
-    except OSError:
-        pass
-    return ""
+    return read_env_file_value(key, install_dir)
 
 
 def _build_uuid_service_map(assignment: dict) -> dict[str, list[str]]:

Light-Heart-Labs/Lighthouse-AI

ods/extensions/services/dashboard-api/gpu.py
@@ -9,6 +9,7 @@ import subprocess
 from pathlib import Path
 from typing import Optional
 
+from env_file import read_env_file_value_state
 from models import GPUInfo, IndividualGPU
 from host_agent_client import AgentClientError, request_json as request_agent_json
 
@@ -543,14 +544,7 @@ def decode_gpu_assignment() -> Optional[dict]:
 def _read_env_var_from_file_state(key: str) -> tuple[bool, str]:
     """Read a live .env value while preserving present-but-empty assignments."""
     install_dir = os.environ.get("ODS_INSTALL_DIR", os.path.expanduser("~/ods"))
-    env_path = Path(install_dir) / ".env"
-    try:
-        for line in env_path.read_text().splitlines():
-            if line.startswith(f"{key}="):
-                return True, line[len(key) + 1:].strip().strip("\"'")
-    except OSError:
-        pass
-    return False, ""
+    return read_env_file_value_state(key, install_dir)
 
 
 def _read_env_var_from_file(key: str) -> str: